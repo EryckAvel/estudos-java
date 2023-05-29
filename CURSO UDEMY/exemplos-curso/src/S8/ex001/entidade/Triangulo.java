b9fdef09f0e6abeb2b7ac5357cca6edcc59629d8
eryckavel <eryckavel@gmail.com>
2023-05-29 16:32:15 -0300

    Ex001 da S8 Finalizado

---
+++ CURSO UDEMY/exemplos-curso/src/S8/ex001/entidade/Triangulo.java
@@ -0,0 +1,19 @@
+package S8.ex001.entidade;
+
+public class Triangulo {
+
+    //todo Uma entidade com seus atributos
+
+    public double a;
+    public double b;
+    public double c;
+
+    //todo Metodo que retiramos da classe pai para estar atribuida a classe triangulo
+
+    public double area(){
+        double p = (a + b + c)/2.0;
+        return Math.sqrt(p * (p-a) * (p-b) * (p-c));
+    }
+
+
+}
