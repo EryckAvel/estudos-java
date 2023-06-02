71404067bbac1545507e2182159281d3f043016e
eryckavel <eryckavel@gmail.com>
2023-06-02 13:04:30 -0300

    Anotações sobre o For Each

--- CURSO UDEMY/exemplos-curso/src/S10/anotações/application/Program.java
+++ CURSO UDEMY/exemplos-curso/src/S10/anotações/application/Program.java
@@ -20,7 +20,7 @@ public class Program {
         System.out.println("Iniciando Projeto de Estudos do S10!");
         System.out.print("Digite o numero do teste que deseja realizar:");
         num = Integer.parseInt(entrada.nextLine());
-        switch (num){
+        switch (num) {
             case 1:
                 //TODO tipos primitivos e tipos referencias
                 int p = 0;
@@ -33,14 +33,14 @@ public class Program {
                 System.out.print("Digite a quantidade de valores:");
                 num = Integer.parseInt(entrada.nextLine());
                 double[] vetor = new double[num];
-                for (i = 0; i < num; i++){
+                for (i = 0; i < num; i++) {
                     System.out.println("Digite a Altura:");
                     vetor[i] = Double.parseDouble(entrada.nextLine());
                 }
-                for (i = 0; i < num; i++){
+                for (i = 0; i < num; i++) {
                     soma += vetor[i];
                 }
-                media = soma/num;
+                media = soma / num;
                 System.out.printf("A media das altura inseridas e de : %.2f", media);
                 break;
             case 3:
@@ -48,24 +48,42 @@ public class Program {
                 System.out.print("Digite a quantidade de valores:");
                 num = Integer.parseInt(entrada.nextLine());
                 Produto[] produtos = new Produto[num];
-                for (i = 0; i < num; i++){
-                    System.out.println("Produto " + (i+1) + ": ");
+                for (i = 0; i < num; i++) {
+                    System.out.println("Produto " + (i + 1) + ": ");
                     System.out.print("Digite o Nome do Produto:");
                     String nome = entrada.nextLine();
                     System.out.print("Digite o Preço do Produto:");
                     double preco = Double.parseDouble(entrada.nextLine());
                     produtos[i] = new Produto(nome, preco);
                 }
-                for (i = 0; i < num; i++){
+                for (i = 0; i < num; i++) {
                     soma += produtos[i].getPreco();
                 }
-                media = soma/num;
+                media = soma / num;
                 System.out.printf("A media dos preços inseridas e de : %.2f", media);
+                break;
+            case 4:
+                //TODO DEMONSTRAÇÃO DA ULTILIZAÇÃO DE BOXING E UNBOXING
+                int x = 20;
+                Object obj = x;
+                System.out.println(obj);
+                int y = (int) obj;
+                System.out.println(y);
+                // todo O UNBOXIN E BOZIN E FEITO DE FORMA NATURAL E SEM A UTILIZAÇÃO DO CASTIN USANDO O WRAPPER QUE SÃO CLASSES EQUIVALENTES AOS TIPOS PRIMITIVOS
+                Integer Z = 20;
+                break;
+            case 5:
+                String[] vect = new String[] {"Marta", "Alex", "Nicolas", "Duda"};
+                System.out.println("RESULTADO USANDO UM FOR NORMAL:");
+                for(i = 0; i < vect.length;i++){
+                    System.out.println(vect[i]);
+                }
+                System.out.println("RESULTADTO USANDO UM FOR EACH:");
+                for (String nomes : vect) {
+                    System.out.println(nomes);
+                }
                 break;
         }
 
-
     }
-
-
 }
