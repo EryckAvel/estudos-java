e705bf51e8af2297ea08eb7d94c4fa5c5025db2e
eryckavel <eryckavel@gmail.com>
2023-06-23 14:39:57 -0300

    Inicio do Projeto Xadrez do S16

---
+++ CURSO UDEMY/exemplos-curso/src/S16D/application/ProgramXadrez.java
@@ -0,0 +1,12 @@
+package S16D.application;
+
+import S16D.model.tabuleiro.Posicao;
+import S16D.model.tabuleiro.Tabuleiro;
+
+public class ProgramXadrez {
+
+    public static void main(String[] args) {
+        var tabuleiro = new Tabuleiro(8,8);
+    }
+
+}
